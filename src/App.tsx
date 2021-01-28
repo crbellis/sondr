3250d3827006ae133eef9d5c9fce834f08112e51
Chase Bellisime <Bellischase@gmail.com>
2021-01-27 22:58:21 -0800

    added signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React from "react";
 import "./App.css";
 import Home from "./Components/Home/Home";
+import Header from "./Components/Header/Header";
+import Signup from "./Components/Signup/Signup";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </BrowserRouter>
-        </div>
-    );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Header />
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route exact path="/signup">
+						<Signup />
+					</Route>
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Components/Signup/Signup.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import FadeIn from "../HOC/FadeIn";
+import classes from "./Signup.module.css";
+
+const Signup = () => {
+	return (
+		<div className={classes.Wrapper}>
+			<FadeIn>
+				<div className={classes.SignupWrapper}>
+					<section className={classes.Section}>
+						<div className={classes.SignupColumn}>
+							<div className={classes.Info}>
+								<div className={classes.Logo}>SONDR</div>
+								<h2 className={classes.SubHeader}>
+									SONDR is currently in development.
+								</h2>
+								<h5 className={classes.TagLine}>
+									Sign up to be notified for updates!
+								</h5>
+							</div>
+							<div className={classes.CardWrapper}>
+								<div className={classes.Card}>
+									<div className={classes.Form}>
+										<div className={classes.Label}>
+											Email:
+										</div>
+										<input placeholder="example@email.com" />
+										<button type="submit">Submit</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
+				</div>
+			</FadeIn>
+		</div>
+	);
+};
+
+export default Signup;
